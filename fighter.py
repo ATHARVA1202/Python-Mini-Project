b4fe34f770c51121dd13170d5c471e446e5db077
Sahil.chauhan <101429324+sahilmc@users.noreply.github.com>
2022-07-05 19:20:19 +0530

    Update fighter.py

--- fighter.py
+++ fighter.py
@@ -3,7 +3,15 @@ import pygame
  
 
 class Fighter():
-    def __init__ (self, x ,y):                    # x and y are coordinates (location)
+    def __init__ (self, x ,y, flip, data, sprite_sheet, animation_steps):                    # x and y are coordinates (location)
+        self.size = data[0]
+        self.image_scale = data[1]
+        self.offset = data[2]
+        self.flip = flip     
+        self.animation_list = self.load_images(sprite_sheet, animation_steps)
+        self.action = 0    # 0 : run 2:jump 3:attack1 4:attack2 5:hit 6:death
+        self.frame_index = 0
+        self.image = self.animation_list[self.action][self.frame_index]
         self.rect = pygame.Rect((x , y, 120, 300))           # 120 = width , 220 = height.
         self.vel_y = 0                                  # for jumping.
         self.jump = False
@@ -11,6 +19,21 @@ class Fighter():
         self.attack_type = 0
         self.health = 100       #player health
         self.flip = False
+
+    def load_images(self, sprite_sheet, animation_steps):
+        #extract images from spritesheet.
+        animation_list = []
+        for y, animation in enumerate(animation_steps):
+            temp_img_list = []
+            for x in range(animation):
+                 temp_img = sprite_sheet.subsurface(x*self.size, y*self.size, self.size, self.size)
+              #   pygame.transform.scale(temp_img, (self.size * self.image_scale,self.size * self.image_scale))  #size of character
+                 temp_img_list.append(pygame.transform.scale(temp_img, (self.size * self.image_scale,self.size * self.image_scale)))
+            animation_list.append(temp_img_list)
+        return animation_list 
+            
+        
+        
         
     def move(self, screen_width, screen_height, surface, target):
         SPEED = 10                       # players speed.
@@ -81,6 +104,8 @@ class Fighter():
         
 
     def draw(self, surface):
+        img = pygame.transform.flip(self.image, self.flip, False)
         pygame.draw.rect(surface, (255, 0, 0), self.rect)           # creating rectangle on game window.
+        surface.blit(img, (self.rect.x - (self.offset[0] * self.image_scale), self.rect.y - (self.offset[1] * self.image_scale)))
 
  
