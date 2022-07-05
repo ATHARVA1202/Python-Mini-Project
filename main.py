a74d945817e01bb10b75109cb06950b51b0efd3b
Sahil.chauhan <101429324+sahilmc@users.noreply.github.com>
2022-07-05 19:18:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from re import T
 import pygame
 from fighter import Fighter
 
@@ -15,8 +16,27 @@ pygame.display.set_caption("")                                    # title for ga
 clock = pygame.time.Clock()
 FPS = 60
 
+# define fighter variables.
+WARRIOR_SIZE = 162
+WARRIOR_SCALE = 4
+WARRIOR_OFFSET = [72, 46]
+WARRIOR_DATA = [WARRIOR_SIZE, WARRIOR_SCALE, WARRIOR_OFFSET]
+WIZARD_SIZE = 250
+WIZARD_SCALE = 3
+WIZARD_OFFSET = [112, 97]
+WIZARD_DATA = [WIZARD_SIZE, WIZARD_SCALE, WIZARD_OFFSET]
+
+
 # load background image.
 background_image = pygame.image.load("mini_project/images/background/background2.jpg").convert_alpha()
+
+#load spritesheets.
+warrior_sheet = pygame.image.load("mini_project/images/warrior/sprites/warrior.png").convert_alpha()
+wizard_sheet = pygame.image.load("mini_project/images/wizard/sprites/wizard.png").convert_alpha()
+
+# define number of steps in each animation.
+WARRIOR_ANIMATION_STEPS = [10, 8, 1, 7, 7, 3, 7]
+WIZARD_ANIMATION_STEPS = [8, 8, 1, 8, 8, 3, 7]
 
 # function for drawing background image.
 def draw_background():
@@ -30,7 +50,7 @@ def health_bar(health, x, y):
     health_drop = health /100
     pygame.draw.rect(screen,(0,0,0),(x-1.5, y-1.5 , 554 ,54))
     pygame.draw.rect(screen,(255,0,0),(x, y , 550 ,50))
-    pygame.draw.rect(screen,(255,255,255),(x, y , 550 * health_drop,50))
+    pygame.draw.rect(screen,(0,255,0),(x, y , 550 * health_drop,50))
 
 
 
@@ -38,8 +58,8 @@ def health_bar(health, x, y):
 
 
 # creating two fighters.
-fighter_1 = Fighter(150, 400)          # (x, y)
-fighter_2 = Fighter(950, 400)
+fighter_1 = Fighter(150, 400, False, WARRIOR_DATA, warrior_sheet, WARRIOR_ANIMATION_STEPS)          # (x, y)
+fighter_2 = Fighter(950, 400, True, WIZARD_DATA, wizard_sheet, WIZARD_ANIMATION_STEPS)
 
 # game loop
 run = True  
@@ -72,3 +92,4 @@ while run:
 
 # exit pygame
 pygame.quit()
+
